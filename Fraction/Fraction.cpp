04052d6987706387a83ebdec1bbb682cd372e881
m0637 <m0637@CHUPAPI>
2025-09-04 19:05:39 +0300

    Создание класса дробь с полями, методами, конструкторами и аксессорами

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Fraction {
+    int numerator;
+    int denominator;
+public:
+    int GetNumerator() {
+        return numerator;
+    }
+    void SetNumerator(int a) {
+        numerator = a;
+    }
+    int GetDenominator() {
+        return denominator;
+    }
+    void SetDenominator(int b) {
+        if (b > 0) {
+            denominator = b;
+        }
+    }
+    void Print() {
+        cout << "Numerator = " << numerator << "\t" << "Denominator = " << denominator << endl;
+    }
+    void Init(int n, int d) {
+        if (d > 0) denominator = d;
+        numerator = n;
+    }
+    void Init() {
+        numerator = 1;
+        denominator = 1;
+    }
+    void Sum(Fraction& fr) {
+        numerator = numerator * fr.denominator + fr.numerator * denominator;
+        denominator = denominator * fr.denominator;
+    }
+    void Subraction(Fraction& fr) {
+        numerator = numerator * fr.denominator - fr.numerator * denominator;
+        denominator = denominator * fr.denominator;
+    }
+};
+
+int main() {
+
+}
